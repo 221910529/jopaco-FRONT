cb517aeacda4db17736f59ff1c1d5957a34de03c
Angel Palacios Mirafuentes <al221910938@gmail.com>
2022-02-25 17:57:36 -0600

    Nav

--- src/Js/NavBar.js
+++ src/Js/NavBar.js
@@ -3,13 +3,34 @@ import "../Css/Botones.css";
 function NavBar() {
   return (
     <div>
-      <BotonOut name="Sara" />
+      <div class="nav">
+        <Inicio name="Inicio" />
+        <Logo />
+        <BotonEx name="Registrarse" />
+        <BotonEx name="Registrarse" />
+        <BotonEx name="Registrarse" />
+        <BotonEx name="Registrarse" />
+        <BotonOut name="Iniciar Sesion" />
+        <BotonOut name="Registrarse" />
+      </div>
     </div>
   );
 }
 
-function BotonOut() {
-  return <button className="btn_Out">Iniciar Sesion</button>;
+function BotonOut(props) {
+  return <button className="btn_Out">{props.name}</button>;
+}
+
+function BotonEx(props) {
+  return <button className="btn_Ex">{props.name}</button>;
+}
+
+function Inicio(props) {
+  return <button className="btn_Out">{props.name}</button>;
+}
+
+function Logo(props) {
+  return "Logo";
 }
 
 export default NavBar;
