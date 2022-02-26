3aa8d51d2317a36ddecb45e38a60d72e518aa881
Angel <al221910938@gmail.com>
2022-02-25 22:37:43 -0600

    crud 2

--- src/Js/Usuarios/altaUsuario.js
+++ src/Js/Usuarios/altaUsuario.js
@@ -1,7 +1,24 @@
 import NavBar from "./NavBar";
 
 function altaUsuarios() {
-  return <div></div>;
+  return (
+    <div>
+      <form action="">
+        <Campo name="Nombre" nombre="nombre"></Campo>
+      </form>
+    </div>
+  );
+}
+
+function Campo(props) {
+  return (
+    <div>
+      Ingrese el campo {props.name}
+      <input type="text" name={props.nombre} id="">
+        {props.name}
+      </input>
+    </div>
+  );
 }
 
 export default altaUsuarios;
