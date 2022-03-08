63b2e0b90068f145a2144d2aadbeda317b63f548
Angel <al221910938@gmail.com>
2022-03-08 17:59:33 -0600

    ver servicios listo

--- src/Js/Servicios/verServicios.js
+++ src/Js/Servicios/verServicios.js
@@ -3,59 +3,69 @@ import React from "react";
 
 import axios from "axios";
 import Cookies from "universal-cookie";
-import { useEffect, useState } from "react";
+import { Component } from "react";
 
 let url = "http://127.0.0.1:8000/api/servicios";
 const cookies = new Cookies();
 
 const token = cookies.get("token");
 
-function VerServicios() {
-  const [APIData, setAPIData] = useState([]);
+class VerServicios extends Component {
+  state = {
+    data: [],
+  };
 
-  useEffect(async () => {
-    await axios //---- mandamos solicitud post al backend
+  componentDidMount() {
+    axios //---- mandamos solicitud post al backend
       .get(url, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       })
-      .then((response) => {
-        setAPIData(response.data);
+      .then((res) => {
+        const usuariosGet = res.data.data;
+        this.setState({
+          data: usuariosGet,
+        });
+        console.log(usuariosGet);
       });
-  }, []);
+  }
 
-  return (
-    <div className="crud">
-      <h1>Ver todos los servicios</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Costo</th>
-            <th>Tiempo</th>
-            <th>Modificar</th>
-            <th>Eliminar</th>
-          </tr>
-        </thead>
-        <tbody>
-          {APIData.map((data) => {
+  render() {
+    return (
+      <div className="crud">
+        <h1>Ver todos los servicios</h1>
+        <table>
+          <thead>
             <tr>
-              <td>{data.id}</td>
-              <td>Maria Anders</td>
-              <td>Germany</td>
-              <td>Francisco Chang</td>
-              <td>Mexico</td>
-            </tr>;
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
-}
-
-function Dato(props) {
-  <tr>{props.name}</tr>;
+              <th>Nombre</th>
+              <th>Costo</th>
+              <th>Tiempo</th>
+              <th>Modificar</th>
+              <th>Eliminar</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.data.map((servicio) => {
+              return (
+                <tr>
+                  <td>{servicio.Nombre_Servicio}</td>
+                  <td>{servicio.Costo}</td>
+                  <td>{servicio.Tiempo_Estimado}</td>
+                  <td>
+                    <button>Modificar</button>
+                  </td>
+                  <td>
+                    <button>Eliminar</button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default VerServicios;
