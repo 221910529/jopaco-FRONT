dcb313ca0ddf8cf03669197cd41451f5439b8c1f
Angel Palacios Mirafuentes <al221910938@gmail.com>
2022-03-25 18:04:30 -0600

    terminar favoritos

--- src/Js/Vista/Base.js
+++ src/Js/Vista/Base.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "../../Css/Inicio.css";
 
-import Carousel from "./Carousel";
+import Carousel from "../Carousel";
 
 export default function Base() {
   return (
     <div className="Inicio">
-      <Carousel/>
+      <Carousel />
       <div className="Bienvenido">Bienvenidos</div>
       <div>Carrousel</div>
       <div>Cards</div>
