f1ebed6826c66956dff897bd32eb3e7b1af9d8fe
LIZARD169 <al221910471@gmail.com>
2022-04-04 00:20:23 -0500

    formato de tablas de altas y consultas

--- src/Js/Servicios/verServicios.js
+++ src/Js/Servicios/verServicios.js
@@ -63,8 +63,8 @@ class VerServicios extends React.Component {
                 <td>
                   <img
                     src={"http://127.0.0.1:8000/img/" + servicio.Foto}
-                    width="150"
-                    heigth="150"
+                    width="80"
+                    heigth="80"
                   />
                 </td>
                 <td>
